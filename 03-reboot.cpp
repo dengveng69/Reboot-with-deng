0b25af7802e6222a046ad6cb687b427e6575e76a
dengveng69 <dengveng69@gmail.com>
2024-02-04 15:01:08 +0530

    Add feature

--- 03-reboot.cpp
+++ 03-reboot.cpp
@@ -6,5 +6,31 @@ int main()
     vector<int> v(10,3);
     vector<int> vc1(v.begin(),v.end());
     vector<int> vc2(vc2);
+
+    vc1.push_back(10);
+    vc1.pop_back();
+
+    vc2.emplace_back(23);
+
+    // lower bound. upper bound
+
+    // swap swap(v1,v2)
+
+    // begin(), end(), rbegin(), rend()
+
+    // to defining 2D vectors
+
+    vector<vector<int>> vec;
+
+    for(auto i = 0; i < vec.size(); i++)
+    {
+        for(auto j = 0; j < vec[i].size(); j++)
+        {
+            cout << vec[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
     return 0;
 }
